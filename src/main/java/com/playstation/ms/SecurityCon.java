368d4255d486eb080353f4f49eafefd54f204cce
Ahmed Mamdouh <43514480+pevolution-ahmed@users.noreply.github.com>
2021-01-02 08:49:08 +0200

    Delete SecurityCon.java

--- src/main/java/com/playstation/ms/SecurityCon.java
+++
@@ -1,7 +0,0 @@
-package com.playstation.ms;
-
-import org.springframework.security.authentication.jaas.SecurityContextLoginModule;
-
-public class SecurityCon extends SecurityContextLoginModule {
-
-}
